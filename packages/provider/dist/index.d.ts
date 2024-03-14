11e4ae890deb88e5661d0ef5ff74d4d47aa5a755
GabrielMartinezRodriguez <gabrielmartinezrdrgz@gmail.com>
2024-03-14 11:51:45 +0100

    chore: remove dist

--- packages/provider/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-export declare class StabilityGtnRpcProvider extends ethers.providers.JsonRpcProvider {
-    constructor(apiKey: string);
-}
-export declare class StabilityTestnetRpcProvider extends ethers.providers
-    .JsonRpcProvider {
-    constructor();
-}
