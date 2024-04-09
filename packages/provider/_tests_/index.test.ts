4a0cbd6f476bff2a6f396bfdc85bd879108441fe
Daniel Blanco Parla <dblancoparla@gmail.com>
2024-04-09 12:12:32 -0700

    refactor: provider tests and viemSetup in provider package

--- packages/provider/_tests_/index.test.ts
+++ packages/provider/_tests_/index.test.ts
@@ -1,45 +1,60 @@
-import { ethers } from 'ethers';
-import { Blockchains, networks } from '../../config/src/index';
+import { ethers } from "ethers";
+import { Blockchains, networks } from "@stabilityprotocol/config";
+import {
+  createStabilityTestnetClientViem,
+  createStabilityGtnClientViem,
+} from "../src/index";
+import { PublicClient } from "viem";
 
-describe('Blockchain RPC Connection and Chain ID Tests', () => {
-    Object.values(Blockchains).forEach((blockchain) => {
-        describe(`${blockchain} Network`, () => {
-            let provider: ethers.JsonRpcProvider;
+describe("Blockchain RPC Connection and Chain ID Tests", () => {
+  Object.values(Blockchains).forEach((blockchain) => {
+    describe(`${blockchain} Network`, () => {
+      let provider: ethers.JsonRpcProvider;
+      let viemProvider: PublicClient;
 
-            beforeAll(() => {
-                const rpcUrl = networks[blockchain].rpcUrls.default.http[0];
-                provider = new ethers.JsonRpcProvider(rpcUrl);
-            });
+      beforeAll(() => {
+        const rpcUrl = networks[blockchain].rpcUrls.default.http[0];
+        provider = new ethers.JsonRpcProvider(rpcUrl);
+        viemProvider = rpcUrl.includes("gtn")
+          ? createStabilityGtnClientViem("apiKey")
+          : createStabilityTestnetClientViem();
+      });
 
-            test('should connect to RPC network', async () => {
-                const network = await provider.getNetwork();
-                expect(network).toBeDefined();
-            });
+      test("should connect to RPC network", async () => {
+        const network = await provider.getNetwork();
+        const viemNetwork = viemProvider.name;
+        expect(network).toBeDefined();
+        expect(viemNetwork).toBeDefined();
+      });
 
-            test('should retrieve the correct chain ID', async () => {
-                const network = await provider.getNetwork();
-                expect(Number(network.chainId)).toBe(networks[blockchain].id);
-            });
+      test("should retrieve the correct chain ID", async () => {
+        const network = await provider.getNetwork();
+        const viemNetwork = await viemProvider.getChainId();
+        expect(Number(network.chainId)).toBe(networks[blockchain].id);
+        expect(Number(viemNetwork)).toBe(networks[blockchain].id);
+      });
 
-            test('should be able to send a simple query to the network', async () => {
-                const latestBlock = await provider.getBlockNumber();
-                expect(latestBlock).toBeGreaterThan(0);
-            });
+      test("should be able to send a simple query to the network", async () => {
+        const latestBlock = await provider.getBlockNumber();
+        expect(latestBlock).toBeGreaterThan(0);
+      });
 
-            test('should have a valid block explorer URL', async () => {
-                const blockExplorerUrl = networks[blockchain].blockExplorers.default.url;
-                expect(blockExplorerUrl).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/); 
-            });
+      test("should have a valid block explorer URL", async () => {
+        const blockExplorerUrl =
+          networks[blockchain].blockExplorers.default.url;
+        expect(blockExplorerUrl).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
+      });
 
-            test('should have a valid logo URI', async () => {
-                const logoUri = networks[blockchain].logoUri;
-                expect(logoUri).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/); 
-            });
+      test("should have a valid logo URI", async () => {
+        const logoUri = networks[blockchain].logoUri;
+        expect(logoUri).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
+      });
 
-            test('should have a valid multicall contract address', async () => {
-                const multicallAddress = networks[blockchain].contracts.multicall3.address;
-                expect(multicallAddress).toMatch(/^0x[a-fA-F0-9]{40}$/); 
-            });
-        });
+      test("should have a valid multicall contract address", async () => {
+        const multicallAddress =
+          networks[blockchain].contracts.multicall3.address;
+        expect(multicallAddress).toMatch(/^0x[a-fA-F0-9]{40}$/);
+      });
     });
+  });
 });
--- packages/provider/src/viemSetup.ts
+++ packages/provider/src/viemSetup.ts
@@ -1,25 +1,27 @@
-import { defineChain, createPublicClient, http } from "viem"; 
+import { defineChain, createPublicClient, http } from "viem";
 import { networks, Blockchains } from "@stabilityprotocol/config";
 
 export const networksConfig = {
-  [Blockchains.STABILITY_TESTNET]: defineChain(networks[Blockchains.STABILITY_TESTNET]),
-  [Blockchains.STABILITY_GTN]: defineChain(networks[Blockchains.STABILITY_GTN]),
+  [Blockchains.STABILITY_TESTNET]: defineChain(
+    networks[Blockchains.STABILITY_TESTNET] as unknown as any
+  ),
+  [Blockchains.STABILITY_GTN]: defineChain(
+    networks[Blockchains.STABILITY_GTN] as unknown as any
+  ),
 };
 
 export const createStabilityTestnetClientViem = () => {
-    return createPublicClient({
-      chain: networks.stabilitytestnet,
-      transport: http(
-        networks[Blockchains.STABILITY_TESTNET].rpcUrls.default.http[0]
-      ),
-    });
-  };
-  
-  export const createStabilityGtnClientViem = (apiKey: string) => {
-    return createPublicClient({
-      chain: networks.stabilitygtn,
-      transport: http(
-        `${networks[Blockchains.STABILITY_GTN].rpcUrls.default.http[0]}/${apiKey}`
-      ),
-    });
-  };
+  return createPublicClient({
+    transport: http(
+      networks[Blockchains.STABILITY_TESTNET].rpcUrls.default.http[0]
+    ),
+  });
+};
+
+export const createStabilityGtnClientViem = (apiKey: string) => {
+  return createPublicClient({
+    transport: http(
+      `${networks[Blockchains.STABILITY_GTN].rpcUrls.default.http[0]}/${apiKey}`
+    ),
+  });
+};
