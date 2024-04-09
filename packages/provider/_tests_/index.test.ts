08e35deca99c1b2141e1fa6551fd92a9a222ca2f
unknown <adam@stabilityprotocol.com>
2024-04-09 16:37:59 -0400

    feat: organize and update unit testing, adjust config to use any for string -> address conversions

--- packages/provider/_tests_/index.test.ts
+++ packages/provider/_tests_/index.test.ts
@@ -20,6 +20,27 @@ describe("Blockchain RPC Connection and Chain ID Tests", () => {
           : createStabilityTestnetClientViem();
       });
 
+      // Config Validation Tests
+
+      test("should have a valid block explorer URL", async () => {
+        const blockExplorerUrl =
+          networks[blockchain].blockExplorers.default.url;
+        expect(blockExplorerUrl).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
+      });
+
+      test("should have a valid logo URI", async () => {
+        const logoUri = networks[blockchain].logoUri;
+        expect(logoUri).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
+      });
+
+      test("should have a valid multicall contract address", async () => {
+        const multicallAddress =
+          networks[blockchain].contracts.multicall3.address;
+        expect(multicallAddress).toMatch(/^0x[a-fA-F0-9]{40}$/);
+      });
+
+      // RPC Connection Tests
+
       test("should connect to RPC network", async () => {
         const network = await provider.getNetwork();
         const viemNetwork = viemProvider.name;
@@ -37,23 +58,32 @@ describe("Blockchain RPC Connection and Chain ID Tests", () => {
       test("should be able to send a simple query to the network", async () => {
         const latestBlock = await provider.getBlockNumber();
         expect(latestBlock).toBeGreaterThan(0);
+        const viemLatestBlock = await viemProvider.getBlockNumber();
+        expect(viemLatestBlock).toBeGreaterThan(0);
       });
 
-      test("should have a valid block explorer URL", async () => {
-        const blockExplorerUrl =
-          networks[blockchain].blockExplorers.default.url;
-        expect(blockExplorerUrl).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
+      test("should retrieve a block by number", async () => {
+        const blockNumber = await provider.getBlockNumber();
+        const block = await provider.getBlock(blockNumber - 1);
+        expect(block).toBeDefined();
+
+        const viemBlockNumber = await viemProvider.getBlockNumber();
+        const viemBlock = await Number(
+          viemProvider.getBlock({
+            blockNumber: viemBlockNumber - BigInt(1),
+            includeTransactions: true,
+          })
+        );
+        expect(viemBlock).toBeDefined();
       });
 
-      test("should have a valid logo URI", async () => {
-        const logoUri = networks[blockchain].logoUri;
-        expect(logoUri).toMatch(/^https?:\/\/[^\s$.?#].[^\s]*$/);
-      });
+      test("should retrieve transaction count by address", async () => {
+        const address = "0xC26CeeFd4e58288e44CDC445D23D43D5202983f9";
+        const txCount = await provider.getTransactionCount(address);
+        expect(txCount).toBeGreaterThanOrEqual(0);
 
-      test("should have a valid multicall contract address", async () => {
-        const multicallAddress =
-          networks[blockchain].contracts.multicall3.address;
-        expect(multicallAddress).toMatch(/^0x[a-fA-F0-9]{40}$/);
+        const viemTxCount = await viemProvider.getTransactionCount({ address });
+        expect(viemTxCount).toBeGreaterThanOrEqual(0);
       });
     });
   });
